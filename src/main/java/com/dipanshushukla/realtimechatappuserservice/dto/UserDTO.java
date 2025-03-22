c8811faf42f4fd28346c1c37f2816beb0636af86
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2025-03-22 07:25:56 +0530

    feat: add builder support to UserDTO

--- src/main/java/com/dipanshushukla/realtimechatappuserservice/dto/UserDTO.java
+++ src/main/java/com/dipanshushukla/realtimechatappuserservice/dto/UserDTO.java
@@ -5,12 +5,14 @@ import java.util.UUID;
 import com.dipanshushukla.realtimechatappuserservice.entity.User;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UserDTO {
     private UUID userId;
 
