51f1a89144d3c7300f5a96b11d4f3cae4b2d67ff
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2025-07-19 14:01:14 +0530

    fix: make fullName field 'not null' in User entity

--- src/main/java/com/dipanshushukla/realtimechatappuserservice/entity/User.java
+++ src/main/java/com/dipanshushukla/realtimechatappuserservice/entity/User.java
@@ -24,6 +24,7 @@ public class User {
     @Column(nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String fullName;
     private String avatar;
     private String bio;
